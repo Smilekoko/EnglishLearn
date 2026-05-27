9f5a728fe194025600764c363ce6965a5c4662c3
Smilekoko <qq179621228@gmail.com>
2026-05-27 10:43:03 +0800

    [添加]元音部分的音标

---
+++ app/src/main/java/com/koko/englishlearn/data/DataPhonetic.kt
@@ -0,0 +1,12 @@
+package com.koko.englishlearn.data
+
+//应标
+//单元音(短元音)
+const val vowels_ɪ = "ɪ"//sit
+const val vowels_e = "e"//bed
+const val vowels_æ = "æ"//cat
+const val vowels_uk_ɒ = "ɒ"//hot,读起来像 "霍特"（偏“奥”的音）
+const val vowels_us_ɑ = "ɑ"//hot,读起来像 "哈特"（偏“啊”的音）
+const val vowels_us_ʌ = "ʌ"//run
+const val vowels_us_ʊ = "ʊ"//put
+const val cvowels_us_ə = "ə"//away
